256ca724da3655377915dabb77e230b9085cd64d
Christopher Marques Corrêa <40878232+ChristopherMarques@users.noreply.github.com>
2022-08-27 00:34:59 -0300

    ✨ update loading screen

--- pages/components/LoadingAnimated/LoadingAnimated.tsx
+++ pages/components/LoadingAnimated/LoadingAnimated.tsx
@@ -2,7 +2,7 @@ const LoadingAnimated = () => {
   return (
     <div role="status">
       <svg
-        className="inline mr-2 w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="inline mr-2 w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-brand-100"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
